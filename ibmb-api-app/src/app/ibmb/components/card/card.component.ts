0c0f61ea5625f6cf7179ee2d907620de941185d3
diana2886 <tdm2886@gmail.com>
2022-11-14 23:29:34 +0600

    feat: add card component layout

--- ibmb-api-app/src/app/ibmb/components/card/card.component.ts
+++ ibmb-api-app/src/app/ibmb/components/card/card.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IMovie } from '../../models/movies.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CardComponent {
+  @Input() movie!: IMovie;
 }
