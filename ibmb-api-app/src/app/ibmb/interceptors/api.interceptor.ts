2592cefa819f8208da91b43f8264beb7de96814d
diana2886 <tdm2886@gmail.com>
2022-11-14 23:36:13 +0600

    refactor: change api key

--- ibmb-api-app/src/app/ibmb/interceptors/api.interceptor.ts
+++ ibmb-api-app/src/app/ibmb/interceptors/api.interceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export default class ApiInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const API_KEY = 'k_9x2ciehs';
+    const API_KEY = 'k_gs64lbhd';
     const URL = 'https://imdb-api.com/en/API/Top250Movies';
 
     return next.handle(
