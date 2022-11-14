1a367353e2f51520d3144da8ccab76d5f369514d
diana2886 <tdm2886@gmail.com>
2022-11-14 16:52:44 +0600

    feat: add api interceptor

---
+++ ibmb-api-app/src/app/ibmb/interceptors/api.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export default class ApiInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const API_KEY = 'k_9x2ciehs';
+    const URL = 'https://imdb-api.com/en/API/Top250Movies';
+
+    return next.handle(
+      request.clone({
+        url: `${URL}/${API_KEY}/${request.url}`,
+      })
+    );
+  }
+}
