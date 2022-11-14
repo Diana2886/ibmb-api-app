b10e3259c87e9a43c474801a787d08cb826e5d64
diana2886 <tdm2886@gmail.com>
2022-11-14 18:06:08 +0600

    feat: add app module settings

--- ibmb-api-app/src/app/app.module.ts
+++ ibmb-api-app/src/app/app.module.ts
@@ -8,20 +8,36 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { CardComponent } from './ibmb/components/card/card.component';
+import { MoviesPageComponent } from './ibmb/pages/movies-page/movies-page.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { appReducer } from './redux/reducers/app.reducer';
+import MoviesEffects from './redux/effects/movies.effects';
+import ApiInterceptor from './ibmb/interceptors/api.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
+    MoviesPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
+    HttpClientModule,
+    StoreModule.forRoot(appReducer, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+        strictActionWithinNgZone: true,
+        strictActionTypeUniqueness: true,
+      },
+    }),
+    EffectsModule.forRoot([MoviesEffects]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
