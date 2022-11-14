d7f1accc6bf165ae43473f3553991eb5de1c6805
diana2886 <tdm2886@gmail.com>
2022-11-14 23:33:14 +0600

    feat: add movies-page layout with logic

--- ibmb-api-app/src/app/ibmb/pages/movies-page/movies-page.component.ts
+++ ibmb-api-app/src/app/ibmb/pages/movies-page/movies-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { fetchMovies } from 'src/app/redux/actions/movies.actions';
+import { selectMovies } from 'src/app/redux/selectors/movies.selectors';
+import { IMovie } from '../../models/movies.model';
 
 @Component({
   selector: 'app-movies-page',
@@ -6,10 +11,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-page.component.scss']
 })
 export class MoviesPageComponent implements OnInit {
+  movies$ = new Observable<IMovie[]>();
 
-  constructor() { }
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
+    this.store.dispatch(fetchMovies());
+    this.movies$ = this.store.select(selectMovies);
+    this.store.dispatch(fetchMovies());
+    this.store.select(selectMovies);
   }
-
 }
